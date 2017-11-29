f4d5beaefe72dbb50f1dddcd7168d046bcfb8fb5
Bo Liu <bloiu@microsoft.com>
2017-11-28 16:28:51 -0800

    add model

---
+++ app/src/main/java/listdemo/boliu/com/listdemo/model/ContactList.java
@@ -0,0 +1,24 @@
+package listdemo.boliu.com.listdemo.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by bloiu on 11/28/2017.
+ */
+public class ContactList {
+
+    @SerializedName("contacts")
+    @Expose
+    private List<Contact> contacts = null;
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+}
